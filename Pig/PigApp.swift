2dc5017095d6922ab1ec8e057d4824f9df95d67c
Umesh Sarma <usarma1200@gmail.com>
2023-11-27 00:31:44 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Umesh Sarma on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
