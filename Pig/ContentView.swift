7624e4862ff7eca73bd12b57bd1229afc50c8606
Umesh Sarma <usarma1200@gmail.com>
2023-11-27 00:57:06 -0600

    added game functionality

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -29,9 +29,19 @@ struct ContentView: View {
                 CustomText(text: "Turn Score: \(turnScore)")
                 HStack{
                     Button("Roll"){
+                        chooseRandom(times: 3)
+                                                    withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                                                        rotation += 360
+                                                    }
                     }
                     .buttonStyle(CustomButtonStyle())
                     Button("Hold"){
+                        gameScore += turnScore
+                                                    endTurn()
+                                                    withAnimation(.easeInOut(duration: 1)) {
+                                                        rotation += 360
+                                                        
+                                                    }
                     }
                     .buttonStyle(CustomButtonStyle())
                 }
@@ -41,6 +51,29 @@ struct ContentView: View {
             
         }
     }
+    func endTurn(){
+            turnScore = 0
+            randomValue = 0
+        }
+    func chooseRandom(times: Int){
+            if times > 0 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                    randomValue = Int.random(in:1...6)
+                    chooseRandom(times: times - 1)
+                }
+            }
+            if times == 0{
+                if randomValue == 1{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                        endTurn()
+                    }
+                    
+                }
+                else{
+                    turnScore += randomValue
+                }
+            }
+        }
 }
 
 struct CustomText: View{
